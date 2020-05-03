490213a632b03c44fc63dc9af269b7a6d9580aed
Fullek <62626590+Fullek@users.noreply.github.com>
2020-05-03 22:31:29 +0200

    A demo() method added
    The demo() is now responsible for handling InputMismatchExeption

--- Calculation.java
+++ Calculation.java
@@ -1,19 +1,31 @@
+import java.util.InputMismatchException;
+
 public class Calculation {
+
+    InputAmount inputAmount = new InputAmount();
 
     private long fibo1 = 1;
     private long fibo2 = 1;
 
-    void showFirstNumbers() {
+    private void showFirstNumbers() {
         System.out.print(fibo1 + " " + fibo2 + " ");
     }
 
-    void calculationLoop(int amount) {
+    private void calculationLoop(int amount) {
         showFirstNumbers();
-        for (int i = 0; i < amount-2; i++) {
+        for (int i = 0; i < amount - 2; i++) {
             long temp = fibo1 + fibo2;
             fibo1 = fibo2;
             fibo2 = temp;
             System.out.print(fibo2 + " ");
+        }
+    }
+
+    protected void demo() {
+        try {
+            calculationLoop(inputAmount.giveAmount());
+        } catch (InputMismatchException e) {
+            System.out.println("You have input wrong number!");
         }
     }
 
